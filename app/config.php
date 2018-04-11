0de1d36948fcffdf9b17f9a7ebc110f1b663b4bd
jhk5000 <ziobro.ad@gmail.com>
2018-04-11 17:14:51 +0200

    Initial commit

---
+++ app/config.php
@@ -0,0 +1,40 @@
+<?php
+
+$config = array();
+
+$config['title'] = 'Stacje paliw u Bolka';
+
+$config['db']['host']     = 'localhost';
+$config['db']['username'] = 'root';
+$config['db']['password'] = '';
+$config['db']['database'] = 'baza';
+
+define('TIME_NOW', time());
+
+$config['page_url']      = 'http://localhost/station/';
+$config['logo_title']    = 'Bolek Stations';
+$config['account_types'] = array(1 => 'Klient', 2 => 'Kierownik', 3 => "Wlasciciel", 4 => 'Administrator');
+
+$config['pages_groups'] = array(
+	'topics'       => array(2,3,4), //
+	'main'         => array(0,1,2,3,4), //strona głowna - dostęp dla każego
+	'myaccount'    => array(1,2,3,4), // edycja konta -  zostaje
+	'lostpassword' => array(0), // przypomnienie hasła - zostaje dla niezalogowanych
+	'stations'  => array(3,4), //"wydziały" -> stacje
+	'customers'     => array(1,2,3,4), // "studenci" - > klienci
+	'managers'    => array(3,4), // " promotorzy" -> kierownicy
+	'faq'          => array(0,1,2,3,4),
+	'edituser'     => array(3,4), //edycja konto użytkowników
+	'messages'     => array(1,2,3,4), // kontakt pomiedzy użytkownikami
+	'mystation'    => array(1), // "myworks" - > mystation (moja stacja, dla kierownika)
+	'station'      => array(2,3), // "works" - > station (wszystkie stacje koncernu)
+	'discount'   => array(2,3,4), // "mysubjects" -> discount (ustalanie zniżek dla klienta)
+	'companies'       => array(3,4), // "groups" -> companies (przypisanie pracownika klienta biznesowego do jego firmy)
+	'archive'      => array(2,3,4), // zamkniete stacje w archiwum
+);
+
+$config['password']['max_lenght'] = 30;
+$config['password']['min_lenght'] = 6;
+
+$config['login']['min_lenght'] = 5;
+$config['login']['max_lenght'] = 20;
