c17dd26d83a79c780768e08f4abe0f844ad04520
jhk5000 <ziobro.ad@gmail.com>
2018-04-25 16:02:08 +0200

    klasy

--- src/Managers.php
+++ src/Managers.php
@@ -9,4 +9,71 @@
 class Managers
 {
 
+    protected $manager_id;
+    protected $Stations_station_id;
+    protected $Users_user_id;
+
+    /**
+     * Managers constructor.
+     * @param $manager_id
+     * @param $Stations_station_id
+     * @param $Users_user_id
+     */
+    public function __construct($manager_id, $Stations_station_id, $Users_user_id)
+    {
+        $this->manager_id = $manager_id;
+        $this->Stations_station_id = $Stations_station_id;
+        $this->Users_user_id = $Users_user_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getManagerId()
+    {
+        return $this->manager_id;
+    }
+
+    /**
+     * @param mixed $manager_id
+     */
+    public function setManagerId($manager_id)
+    {
+        $this->manager_id = $manager_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStationsStationId()
+    {
+        return $this->Stations_station_id;
+    }
+
+    /**
+     * @param mixed $Stations_station_id
+     */
+    public function setStationsStationId($Stations_station_id)
+    {
+        $this->Stations_station_id = $Stations_station_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsersUserId()
+    {
+        return $this->Users_user_id;
+    }
+
+    /**
+     * @param mixed $Users_user_id
+     */
+    public function setUsersUserId($Users_user_id)
+    {
+        $this->Users_user_id = $Users_user_id;
+    }
+
+
+
 }
--- src/Prices.php
+++ src/Prices.php
@@ -9,4 +9,89 @@
 class Prices
 {
 
+    protected $price_id;
+    protected $Stations_station_id;
+    protected $gas_type;
+    protected $price;
+
+    /**
+     * Prices constructor.
+     * @param $price_id
+     * @param $Stations_station_id
+     * @param $gas_type
+     * @param $price
+     */
+    public function __construct($price_id, $Stations_station_id, $gas_type, $price)
+    {
+        $this->price_id = $price_id;
+        $this->Stations_station_id = $Stations_station_id;
+        $this->gas_type = $gas_type;
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPriceId()
+    {
+        return $this->price_id;
+    }
+
+    /**
+     * @param mixed $price_id
+     */
+    public function setPriceId($price_id)
+    {
+        $this->price_id = $price_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStationsStationId()
+    {
+        return $this->Stations_station_id;
+    }
+
+    /**
+     * @param mixed $Stations_station_id
+     */
+    public function setStationsStationId($Stations_station_id)
+    {
+        $this->Stations_station_id = $Stations_station_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGasType()
+    {
+        return $this->gas_type;
+    }
+
+    /**
+     * @param mixed $gas_type
+     */
+    public function setGasType($gas_type)
+    {
+        $this->gas_type = $gas_type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+
 }
