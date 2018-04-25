b2026570a10f93bd639f86f8633615256fb9d876
jhk5000 <ziobro.ad@gmail.com>
2018-04-25 20:58:22 +0200

    zmiany

--- src/User.php
+++ src/User.php
@@ -16,41 +16,6 @@ class User
      */
     protected $login;
     /**
-     * @Column(type="string")
-     * @var string
-     */
-    protected $passwd;
-    /**
-     * @Column(type="string")
-     * @var string
-     */
-    protected $name;
-    /**
-     * @Column(type="string")
-     * @var string
-     */
-    protected $mail;
-    /**
-     * @Column(type="string")
-     * @var string
-     */
-    protected $register_date;
-    /**
-     * @Column(type="string")
-     * @var string
-     */
-    protected $group_id;
-    /**
-     * @Column(type="string")
-     * @var string
-     */
-    protected $info;
-    /**
-     * @Column(type="string")
-     * @var string
-     */
-    protected $token;
-    /**
      * @OneToMany(targetEntity="Bug", mappedBy="reporter")
      * @var Bug[]
      */
@@ -60,138 +25,32 @@ class User
      * @var Bug[]
      */
     protected $assignedBugs = null;
-
     public function __construct()
     {
         $this->reportedBugs = new ArrayCollection();
         $this->assignedBugs = new ArrayCollection();
     }
-    /**
-     * @return string
-     */
-    public function getUserId()
+
+    public function getId()
     {
         return $this->user_id;
     }
-    /**
-     * @param string $user_id
-     */
-    public function setUserId($user_id)
-    {
-        $this->user_id = $user_id;
-    }
-    /**
-     * @return string
-     */
     public function getLogin()
     {
         return $this->login;
     }
-    /**
-     * @param string $login
-     */
     public function setLogin($login)
     {
         $this->login = $login;
     }
-    /**
-     * @return string
-     */
-    public function getPasswd()
+
+    public function addReportedBug($bug)
     {
-        return $this->passwd;
-    }
-    /**
-     * @param string $passwd
-     */
-    public function setPasswd($passwd)
-    {
-        $this->passwd = $passwd;
-    }
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-    /**
-     * @param string $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-    /**
-     * @return string
-     */
-    public function getMail()
-    {
-        return $this->mail;
-    }
-    /**
-     * @param string $mail
-     */
-    public function setMail($mail)
-    {
-        $this->mail = $mail;
-    }
-    /**
-     * @return string
-     */
-    public function getRegisterDate()
-    {
-        return $this->register_date;
-    }
-    /**
-     * @param string $register_date
-     */
-    public function setRegisterDate($register_date)
-    {
-        $this->register_date = $register_date;
-    }
-    /**
-     * @return string
-     */
-    public function getGroupId()
-    {
-        return $this->group_id;
-    }
-    /**
-     * @param string $group_id
-     */
-    public function setGroupId($group_id)
-    {
-        $this->group_id = $group_id;
-    }
-    /**
-     * @return string
-     */
-    public function getInfo()
-    {
-        return $this->info;
-    }
-    /**
-     * @param string $info
-     */
-    public function setInfo($info)
-    {
-        $this->info = $info;
+        $this->reportedBugs[] = $bug;
     }
 
-    /**
-     * @return string
-     */
-    public function getToken()
+    public function assignedToBug($bug)
     {
-        return $this->token;
-    }
-
-    /**
-     * @param string $token
-     */
-    public function setToken($token)
-    {
-        $this->token = $token;
+        $this->assignedBugs[] = $bug;
     }
 }
