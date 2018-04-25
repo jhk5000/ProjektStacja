3fcf92bce8503602805682be49e751835bec5d2e
jhk5000 <ziobro.ad@gmail.com>
2018-04-25 21:41:17 +0200

    zmiany

--- app/server.php
+++ app/server.php
@@ -1,31 +1,31 @@
 <?php
 session_start();
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors',1);
 require_once('../bootstrap.php');
 require('config.php');
 require('functions.php');
-if (!empty($_POST['task'])) {
+if(!empty($_POST['task'])) {
     $time_now = time();
     //$db = new DB($config['db']['host'], $config['db']['username'], $config['db']['password'], $config['db']['database']);
 
-    if (!empty($_SESSION['user'])) {
-        if (empty($_SESSION['token'])) {
+    if(!empty($_SESSION['user'])) {
+        if(empty($_SESSION['token'])) {
             session_destroy();
             $_SESSION = array();
             die();
         }
         $user = $entityManager->find('User', $_SESSION['user']);
-        if ($user === null || $user->getToken() != $_SESSION['token']) {
+        if($user === null || $user->getToken() != $_SESSION['token']) {
             session_destroy();
             $_SESSION = array();
             die();
         }
     }
-    switch ($_POST['task']) {
+    switch($_POST['task']) {
         case 1:
-            if (!empty($_POST['name'])) {
-                if ($_POST['pass1'] != $_POST['pass2']) {
+            if(!empty($_POST['name'])) {
+                if($_POST['pass1'] != $_POST['pass2']) {
                     echo 'Podane hasła nie są identyczne!';
                 } elseif (!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
                     echo 'Podano błędny adres email!';
@@ -37,7 +37,7 @@ if (!empty($_POST['task'])) {
                     $mail = $entityManager->getRepository('User')->findOneBy(array('mail' => $_POST['mail']));
                     if ($user !== null) {
                         echo 'Podany login jest już zajęty!';
-                    } elseif ($mail !== null) {
+                    } elseif($mail !== null){
                         echo 'Podany mail jest już zajęty!';
                     } else {
                         $password = password_hash($_POST['pass1'], PASSWORD_BCRYPT);
@@ -63,7 +63,7 @@ if (!empty($_POST['task'])) {
             }
             break;
         case 2:
-            if (!empty($_POST['login']) && !empty($_POST['password'])) {
+            if(!empty($_POST['login']) && !empty($_POST['password'])) {
                 $login = $_POST['login'];
                 $user = $entityManager->getRepository('User')->findOneBy(array('login' => $login));
 //                    $user = $entityManager->find('User', 1);
@@ -72,14 +72,14 @@ if (!empty($_POST['task'])) {
                     exit(1);
                 }
                 $password = $user->getPasswd();
-                if (password_verify($_POST['password'], $password)) {
-                    $_SESSION['user'] = $user->getUserId();
+                if(password_verify($_POST['password'], $password)){
+                    $_SESSION['user']  = $user->getUserId();
                     $token = generateToken();
                     $_SESSION['token'] = $token;
                     $user->setToken($token);
                     $entityManager->flush();
                     echo 1;
-                } else {
+                }else{
                     echo "Podano nieporawidłowe hasło!";
                 }
             } else {
@@ -89,7 +89,7 @@ if (!empty($_POST['task'])) {
         case 3:
             if ($_POST['window'] == 1) {
                 echo getLoginWindow();
-            } else if ($_POST['window'] == 3) {
+            } else if($_POST['window'] == 3) {
                 $subjects = $db->select_multi('SELECT * FROM studies');
                 echo getSubjectWindow($subjects);
             } else {
