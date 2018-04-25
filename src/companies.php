edfb41d0f0ec47fcd1fc2b90a8cd95f2ed4dd880
jhk5000 <ziobro.ad@gmail.com>
2018-04-25 17:40:26 +0200

    companies

---
+++ src/companies.php
@@ -0,0 +1,50 @@
+﻿<?php
+
+class Companies
+{
+    protected $Users_user_id;
+    protected $company;
+
+    /**
+     * Companies constructor.
+     * @param $Users_user_id
+     * @param $company
+     */
+    public function __construct($Users_user_id, $company)
+    {
+        $this->Users_user_id = $Users_user_id;
+        $this->company = $company;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsersUserId()
+    {
+        return $this->Users_user_id;
+    }
+
+    /**
+     * @param mixed $User_user_id
+     */
+    public function setUsersUserId($Users_user_id)
+    {
+        $this->Users_user_id = $Users_user_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * @param mixed $company
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+    }
+}
