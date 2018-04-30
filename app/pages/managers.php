b29f47d0b74369e3b3d6b406eb7cbbea673adeb9
Rafal_2 <rafal90906@gmail.com>
2018-04-30 22:46:33 +0200

    przydzielanie kierowników do stacji

--- app/pages/managers.php
+++ app/pages/managers.php
@@ -7,6 +7,8 @@ $managers = $userRepository->findBy(array('group_id' => '2'));
 
 $query = $entityManager->createQuery("SELECT u.login, u.name, u.mail, u.register_date, s.station_name, s.city, s.street, s.voivodeship, u.user_id, s.station_id, m.manager_id, m.Users_user_id, m.Stations_station_id FROM User u, Stations s, Managers m where u.user_id = m.Users_user_id AND m.Stations_station_id = s.station_id AND u.group_id = 2");
 $results = $query->getResult();
+$query1 = $entityManager->createQuery("SELECT u.login, u.name, u.mail, u.register_date, u.user_id FROM User u WHERE u.user_id NOT IN (SELECT m.Users_user_id FROM Managers m) AND u.group_id = 2");
+$results1 = $query1->getResult();
 $l=1;
 	echo '<table class="table table-bordered table-hover">
 			<thead>
@@ -35,12 +37,42 @@ foreach ($results as $manager) {
             <td>'.$manager['city'].',</br>'.$manager['street'].',</br>'.$manager['voivodeship'].'</td>
             <td>
                 <center>
-                    <a href="'.$config['page_url'].'?page=edituser&id='.$manager['user_id'].'"><i class=\'glyphicon glyphicon-pencil\'></i></a> 
+                    <a href="'.$config['page_url'].'?page=editmanager&id='.$manager['user_id'].'"><i class=\'glyphicon glyphicon-pencil\'></i></a> 
                     <a href="javascript://" title=\'<center>Czy napewno chcesz usunąć użytkownika '.$manager['login'].'?</center>\' data-placement="bottom" data-html=\'true\' data-toggle="popover" data-trigger="focus" 
                     data-content="
                         <form method=\'post\' enctype=\'multipart/form-data\' action=\'\'>
                             <div class=\'form-group\'>
-                                <center><input type=\'hidden\' name=\'id\' value=\''.$manager['manager_id'].'\'>
+                                <center><input type=\'hidden\' name=\'id\' value=\''.$manager['user_id'].'\'>
+                                    <button type=\'submit\' name=\'deleteManager\' class=\'btn btn-success\'>Tak</button> 
+                                    <button type=\'button\' class=\'btn btn-danger\' data-dismiss=\'modal\'>Nie</button>
+                                </center>
+                            </div>
+                        </form>">
+                    <i class=\'glyphicon glyphicon-trash\' style="color: black;"></i></a>
+                </center>
+            </td>
+        </tr>
+        ';
+    $l++;
+}//end foreach
+foreach ($results1 as $manager) {
+    echo '
+        <tr>
+            <td>'.$l. '</td>
+            <td>'.$manager['login'].'</td>
+            <td>'.$manager['name'].' </td>
+            <td>'.$manager['mail'].'</td>
+            <td>'.$manager['register_date'].'</td>
+            <td></td>
+            <td></td>
+            <td>
+                <center>
+                    <a href="'.$config['page_url'].'?page=editmanager&id='.$manager['user_id'].'"><i class=\'glyphicon glyphicon-pencil\'></i></a> 
+                    <a href="javascript://" title=\'<center>Czy napewno chcesz usunąć użytkownika '.$manager['login'].'?</center>\' data-placement="bottom" data-html=\'true\' data-toggle="popover" data-trigger="focus" 
+                    data-content="
+                        <form method=\'post\' enctype=\'multipart/form-data\' action=\'\'>
+                            <div class=\'form-group\'>
+                                <center><input type=\'hidden\' name=\'id\' value=\''.$manager['user_id'].'\'>
                                     <button type=\'submit\' name=\'deleteManager\' class=\'btn btn-success\'>Tak</button> 
                                     <button type=\'button\' class=\'btn btn-danger\' data-dismiss=\'modal\'>Nie</button>
                                 </center>
@@ -56,7 +88,7 @@ foreach ($results as $manager) {
 echo '</tbody></table>';
 
 if(isset($_POST['deleteManager'])){
-    $deleted = $entityManager->find('Managers', $_POST['id']);
+    $deleted = $entityManager->find('User', $_POST['id']);
     $entityManager->remove($deleted);
     $entityManager->flush();
     echo '<script type="text/javascript">
