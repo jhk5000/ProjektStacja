a427b99fad453d322aed75cf9b29cb9a41993dba
jhk5000 <ziobro.ad@gmail.com>
2018-04-30 13:53:34 +0200

    merge

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 var app = {
-	server: 'app/server.php',
-	promoter_subjects: [],
-	login: function() {
-		var login = document.getElementById('login_name').value;
-		var password = document.getElementById('login_password').value;
-		$.ajax({ type: 'POST', url: app.server, data: {task: 2, login: login, password: password} })
-		.done(function(msg) {
-			if(parseInt(msg) == 1) {
-				location.reload();
-			} else {
-				app.modalAlert(msg);
-			}
-		});
-	},
+    server: 'app/server.php',
+    promoter_subjects: [],
+    login: function() {
+        var login = document.getElementById('login_name').value;
+        var password = document.getElementById('login_password').value;
+        $.ajax({ type: 'POST', url: app.server, data: {task: 2, login: login, password: password} })
+            .done(function(msg) {
+                if(parseInt(msg) == 1) {
+                    location.reload();
+                } else {
+                    app.modalAlert(msg);
+                }
+            });
+    },
     logout: function() {
         $.ajax({ type: 'POST', url: app.server, data: {task: 4} })
             .done(function(msg) {
@@ -24,92 +24,92 @@ var app = {
                 // location.reload();
             });
     },
-	userTypeChange: function() {
-		var type = document.getElementById('user_type').value;
-		if(type == 1) {
-			document.getElementById('student_subject').style.display = 'block';
-			document.getElementById('promoter_subject').style.display = 'none';
-		} else {
-			document.getElementById('student_subject').style.display = 'none';
-			document.getElementById('promoter_subject').style.display = 'block';
-		}
-	},
-	onSubjectChange: function(id) {
-		var b = document.getElementById('subject_'+id).checked;
-		if(b) {
-			this.promoter_subjects.push(id);
-		} else {
-			var a = this.promoter_subjects;
-			var cache = [];
-			var i;
-			var l = a.length;
-			for(i = 0; i < l; i++) {
-				if(a[i] != id) {
-					cache.push(a[i]);
-				}
-			}
-			this.promoter_subjects = cache;
-		}
-		document.getElementById('subject_count').innerHTML = this.promoter_subjects.length;
-	},
-	check_key: function(e) {
-		var unicode = e.keyCode? e.keyCode : e.charCode;
-		if(unicode == 13) {
-			if(this.getElement('loginBox')) this.login();
-			else this.register();
-		}
-	},
-	promoterChooseSubjects: function() {
-		$.ajax({ type: 'POST', url: app.server, data: {task: 3, window: 3} })
-		.done(function(msg) {
-			document.getElementById('box').innerHTML += '<div id="modalWindow"><div id="shadow2" onClick="app.closeModal();"></div>'+msg+'</div>';
-			var l = app.promoter_subjects.length;
-			if(l > 0) {
-				for(var i = 0; i < l; i++) {
-					document.getElementById('subject_'+app.promoter_subjects[i]).checked = true;
-				}
-			}
-		});
-	},
-	register: function() {
-		var login        = document.getElementById('register_login').value;
-		var pass1        = document.getElementById('register_pass1').value;
-		var pass2        = document.getElementById('register_pass2').value;
+    userTypeChange: function() {
+        var type = document.getElementById('user_type').value;
+        if(type == 1) {
+            document.getElementById('student_subject').style.display = 'block';
+            document.getElementById('promoter_subject').style.display = 'none';
+        } else {
+            document.getElementById('student_subject').style.display = 'none';
+            document.getElementById('promoter_subject').style.display = 'block';
+        }
+    },
+    onSubjectChange: function(id) {
+        var b = document.getElementById('subject_'+id).checked;
+        if(b) {
+            this.promoter_subjects.push(id);
+        } else {
+            var a = this.promoter_subjects;
+            var cache = [];
+            var i;
+            var l = a.length;
+            for(i = 0; i < l; i++) {
+                if(a[i] != id) {
+                    cache.push(a[i]);
+                }
+            }
+            this.promoter_subjects = cache;
+        }
+        document.getElementById('subject_count').innerHTML = this.promoter_subjects.length;
+    },
+    check_key: function(e) {
+        var unicode = e.keyCode? e.keyCode : e.charCode;
+        if(unicode == 13) {
+            if(this.getElement('loginBox')) this.login();
+            else this.register();
+        }
+    },
+    promoterChooseSubjects: function() {
+        $.ajax({ type: 'POST', url: app.server, data: {task: 3, window: 3} })
+            .done(function(msg) {
+                document.getElementById('box').innerHTML += '<div id="modalWindow"><div id="shadow2" onClick="app.closeModal();"></div>'+msg+'</div>';
+                var l = app.promoter_subjects.length;
+                if(l > 0) {
+                    for(var i = 0; i < l; i++) {
+                        document.getElementById('subject_'+app.promoter_subjects[i]).checked = true;
+                    }
+                }
+            });
+    },
+    register: function() {
+        var login        = document.getElementById('register_login').value;
+        var pass1        = document.getElementById('register_pass1').value;
+        var pass2        = document.getElementById('register_pass2').value;
         var mail         = document.getElementById('register_mail').value;
         var name         = document.getElementById('register_name').value;
-		var group		 = document.getElementById('user_type').value;
+        var group		 = document.getElementById('user_type').value;
 
-		$.ajax({ type: 'POST', url: app.server, data: {task: 1, name: name, login: login, mail:mail, pass1: pass1, pass2: pass2, type: group} })
-		.done(function(msg) {
-			if(parseInt(msg) == 1) {
-				location.reload();
-			} else {
-				app.modalAlert(msg);
-			}
-		});
-	},
-	modalAlert: function(t) {
-		alert(t);
-	},
-	openWindow: function(i) {
-		$.ajax({ type: 'POST', url: app.server, data: {task: 3, window: i} })
-		.done(function(msg) {
-			if(!app.getElement('modalWindow'))
-				document.getElementById('page').innerHTML += '<div id="modalWindow"><div id="shadow" onClick="app.closeModal();"></div>'+msg+'</div>';
-		});
-	},
-	closeModal: function() {
-		this.removeElement('modalWindow');
-	},
-	removeElement: function(i) {
-		if(this.getElement(i)) {
-			el = document.getElementById(i);
-			el.parentNode.removeChild(el);
-		}
-	},
-	getElement: function(i) {
-		return document.getElementById(i);
-	}
+        $.ajax({ type: 'POST', url: app.server, data: {task: 1, name: name, login: login, mail:mail, pass1: pass1, pass2: pass2, group: group} })
+            .done(function(msg) {
+                if(parseInt(msg) == 1) {
+                    location.reload();
+                } else {
+                    app.modalAlert(msg);
+                }
+            });
+    },
+    modalAlert: function(t) {
+        alert(t);
+    },
+    openWindow: function(i) {
+        $.ajax({ type: 'POST', url: app.server, data: {task: 3, window: i} })
+            .done(function(msg) {
+                if(!app.getElement('modalWindow'))
+                    document.getElementById('page').innerHTML += '<div id="modalWindow"><div id="shadow" onClick="app.closeModal();"></div>'+msg+'</div>';
+            });
+    },
+    closeModal: function() {
+        this.removeElement('modalWindow');
+    },
+    removeElement: function(i) {
+        if(this.getElement(i)) {
+            el = document.getElementById(i);
+            el.parentNode.removeChild(el);
+        }
+    },
+    getElement: function(i) {
+        return document.getElementById(i);
+    }
 };
 
 /*
