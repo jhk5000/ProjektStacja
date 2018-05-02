d0d61c61b721eaff6559d5b1e378c60d679f1171
jhk5000 <ziobro.ad@gmail.com>
2018-05-02 11:19:57 +0200

    zmian discount na companies, poprawienie kluczy i dodanie insrtu do companies

--- stations_db.sql
+++ stations_db.sql
@@ -28,12 +28,20 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `discount`
 --
 
-CREATE TABLE `discount` (
-  `discount_id` int(10) UNSIGNED NOT NULL,
-  `Users_user_id` int(10) UNSIGNED NOT NULL,
+CREATE TABLE `companies` (
+  `company_id` int(10) UNSIGNED NOT NULL,
+  `company_name` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
   `discount` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+--
+-- Zrzut danych tabeli `prices`
+--
 
+INSERT INTO `companies` (`company_id`, `company_name`, `address`, `discount`) VALUES
+(1, 'Our Company', 'Wierzbowa 3, Rzeszów', '90'),
+(2, 'Firma Testowa 1', 'Baziowa 5, Rzeszów', '10');
+(3, 'FirmaTestowa 2', 'Sosnowa 15, Rzeszów', '15');
 -- --------------------------------------------------------
 
 --
@@ -152,7 +160,7 @@ CREATE TABLE `users` (
   `register_date` date DEFAULT NULL,
   `group_id` int(10) UNSIGNED DEFAULT NULL,
   `token` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
-  `company` varchar(255) COLLATE utf8_polish_ci DEFAULT NULL,
+  `Companies_company_id` int(10) UNSIGNED DEFAULT NULL,
   `info` text COLLATE utf8_polish_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
@@ -160,13 +168,13 @@ CREATE TABLE `users` (
 -- Zrzut danych tabeli `users`
 --
 
-INSERT INTO `users` (`user_id`, `login`, `passwd`, `name`, `mail`, `register_date`, `group_id`, `token`, `company`, `info`) VALUES
+INSERT INTO `users` (`user_id`, `login`, `passwd`, `name`, `mail`, `register_date`, `group_id`, `token`, `Companies_company_id`, `info`) VALUES
 (1, 'a', '$2y$10$pfdm9ssnDrQdXlVSSQ0U1OxfSA9fdUYgcP2qB/f0pV68hofjbjdMK', 'a', 'a', '2018-04-09', 4, '$2y$10$34Ocda5LvfuN/Dzp0XlzMe90IkWp1UcmSO9PRzqUm/LCZTBoeI9HW', NULL, 'a'),
 (4, '1', '1', '11', '1@1.pl', '0000-00-00', 2, '1', '1', '0'),
-(7, '4', '4', '4', '4@4.pl', '0000-00-00', 2, '4', '4', '0'),
-(9, '6', '6', '65', '6@6.pl', '0000-00-00', 2, '6', '6', '0'),
-(11, '8', '8', '8', '8@8.pl', '0000-00-00', 1, '8', '8', '0'),
-(12, '9', '9', '9', '9@9.pl', '0000-00-00', 1, '9', '9', '0');
+(7, '4', '4', '4', '4@4.pl', '0000-00-00', 2, '4', '1', '0'),
+(9, '6', '6', '65', '6@6.pl', '0000-00-00', 2, '6', '1', '0'),
+(11, '8', '8', '8', '8@8.pl', '0000-00-00', 1, '8', '2', '0'),
+(12, '9', '9', '9', '9@9.pl', '0000-00-00', 1, '9', '2', '0');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -175,9 +183,8 @@ INSERT INTO `users` (`user_id`, `login`, `passwd`, `name`, `mail`, `register_dat
 --
 -- Indexes for table `discount`
 --
-ALTER TABLE `discount`
-  ADD PRIMARY KEY (`discount_id`),
-  ADD KEY `Discount_FKIndex2` (`Users_user_id`);
+ALTER TABLE `companies`
+  ADD PRIMARY KEY (`company_id`);
 
 --
 -- Indexes for table `event_log`
@@ -218,7 +225,8 @@ ALTER TABLE `stations`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`)
+  ADD KEY `Companies_FKIndex1` (`Companies_company_id`);;
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -227,8 +235,8 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT dla tabeli `discount`
 --
-ALTER TABLE `discount`
-  MODIFY `discount_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Companies`
+  MODIFY `company_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT dla tabeli `event_log`
